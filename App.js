388ae947dd9520fdddd66801b4f19bc2d8512eb1
PritishWa <92771064+PritishWa@users.noreply.github.com>
2023-08-05 18:22:08 +0530

    drawer and botton tab navigation commit

--- App.js
+++ App.js
@@ -1,3 +1,4 @@
+
 import { View, Text } from 'react-native'
 import React from 'react'
 import AppNavigator from './src/navigation/AppNavigator'
--- src/screen/SplashScreen.js
+++ src/screen/SplashScreen.js
@@ -1,17 +1,20 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { ImageBackground, StyleSheet, Text, View } from 'react-native'
 import React, { useEffect } from 'react'
 
 const SplashScreen = ({navigation}) => {
 
     useEffect(()=>{
         setTimeout(()=>{
-            navigation.navigate('DrawerNavigation')
-        },2000)
+            navigation.navigate('FirstScreen')
+        },3000)
     },[])
 
   return (
     <View style={styles.container}>
-      <Text>SplashScreen</Text>
+      <ImageBackground
+        source={require('../assets/images/Splash.png')}
+        style={{width:'100%',height:'100%'}}
+      ></ImageBackground>
     </View>
   )
 }
